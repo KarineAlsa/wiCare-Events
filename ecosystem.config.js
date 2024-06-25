6427549c3ae194dda055c0d6abd9d2f7fcc99fe6
KarineAlsa <saremana@hotmail.com>
2024-06-24 23:19:53 -0600

    events added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'events-api',
-        script: './dist/index.js',
+        script: './dist/app.js',
         watch: true,
         ignore_watch: ["node_modules", "dist"],
         watch_options: {
