fdb5c9b536c9caad63145e7a6fc22ba31012ddf4
KarineAlsa <saremana@hotmail.com>
2024-06-24 23:16:54 -0600

    events added

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [
+      {
+        name: 'events-api',
+        script: './dist/index.js',
+        watch: true,
+        ignore_watch: ["node_modules", "dist"],
+        watch_options: {
+          "followSymlinks": false
+        },
+        env: {
+          NODE_ENV: 'development',
+          PORT: 1234
+        },
+        env_production: {
+          NODE_ENV: 'production',
+          PORT: 1234
+        }
+      }
+    ]
+  };
+  
