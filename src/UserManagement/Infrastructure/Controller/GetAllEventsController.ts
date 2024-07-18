6d4627f6093c88978487a3e91ac73b1b9f585f0b
KarineAlsa <saremana@hotmail.com>
2024-07-18 16:04:40 -0600

    aver

--- src/UserManagement/Infrastructure/Controller/GetAllEventsController.ts
+++ src/UserManagement/Infrastructure/Controller/GetAllEventsController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import  GetAllEventsUseCase  from "../../Application/UseCase/GetAllEventsUseCase";
 import sendMessageAndWaitForResponse from "../Service/SagaMessagin";
 
@@ -12,11 +12,19 @@ export default class GetAllEventsController {
             let events = await this.useCase.run();
 
             console.log(events)
-            let event = await sendMessageAndWaitForResponse("getAllEvents",events)
             
-            if (event) {
-                
-                return response.status(200).json({data:event,message:"All events",success:true});
+            if (events) {
+                let event = await sendMessageAndWaitForResponse("getAllEvents",events)
+                if (event) {
+                    
+                    return response.status(200).json({data:event,message:"All events",success:true});
+                }
+                else {
+                    response.status(404).send({
+                        message: "No se pudo obtener los eventos",
+                        success: false,
+                    });
+                }
             } else {
                 response.status(404).send({
                     
